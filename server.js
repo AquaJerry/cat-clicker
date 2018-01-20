e7fbb8d322a2bd9638ae93c9d73a7e81c33a3b84
aquajerry <huopopo_zeyang@163.com>
2018-01-20 17:49:45 +0800

    fix: "unspected token" in server

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 // (c) 2018 LiuZeyang, Guangzhou University.
 // Cat Clicker Server is freely distributed under the terms of ISC license.
 
-const fs = require('fs');
-const http = require('http');
+const express = require('express');
 const path = require('path');
 
 // Locally run the website.
-fs.readFile(path.resolve('dist/index.html'), (err, html) => {
-    if (err) {
-        throw err;
-    }
-    http.createServer((req, res) => {
-        res.writeHeader(200, {
-            'content-Type': 'text/html',
-        });
-        res.write(html);
-        res.end();
-    }).listen(8080);
-    console.log('Running at http://localhost:8080');
-    console.log('Ctrl+C to exit.');
-});
+express().use(express.static(path.resolve('dist'))).listen(8080);
+
+console.log('Running at http://localhost:8080');
+console.log('Ctrl+C to exit.');
